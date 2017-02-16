64600910106882f00c2ec0fe99e2665508a7b862
Futa Hirakoba <korosuke613613@gmail.com>
2017-02-16 12:37:28 +0900

    Create to top page

--- html/index.php
+++ html/index.php
@@ -19,11 +19,25 @@
     <?php include (dirname(__FILE__).'/parts/menu.html'); ?>
 
     <!-- 本文ブロック -->
-    <div class="container">
+    <div class="container" style="background:#999;height: 100vh;">
         <div class="row">
-            <div class="col-xs-12" style="background:#999;height: 100vh;">
-                <h2>本文ブロック</h2>
-                <div id="sssbarName">ああああああああ</div>
+            <div class="col-sm-5 visible-sm visible-md visible-lg">
+                <p></p>
+            <img src="/images/welcome.jpg" class="img-responsive img-rounded" alt="ひらこばの写真">
+            </div>
+                <div class="col-sm-5 visible-xs">
+                <p></p>
+                <img src="/images/welcome_wide.jpg" class="img-responsive img-rounded" alt="ひらこばの写真">
+            </div>
+            <div class="col-sm-7">
+                <h1>Welcome To Kat-Lab !!</h1>
+                片山(徹)研では， 「ソフトウェア開発における人間の負担をいかに減らし、ソフトウェアの品質を上げることができるのか？」 をテーマに、リファクタリング，形式手法，モデリング言語、テスト等を研究しています。
+            </div>
+        </div>
+        <hr>
+        <div class="col-xs-12">
+            <div>
+                <h1>新着情報</h1>
             </div>
         </div>
     </div>
@@ -33,7 +47,7 @@
 
     <!-- Javascriptの読み込み -->
     <?php include (dirname(__FILE__).'/parts/js_load.html'); ?>
-    
+
 </body>
 
 </html>
