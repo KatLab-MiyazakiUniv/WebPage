7334275d918ce24afe7aafa1613b138ee6202302
Futa Hirakoba <korosuke613613@gmail.com>
2017-02-18 05:47:17 +0900

    メンバー紹介ページを作成

--- html/member.php
+++ html/member.php
@@ -21,8 +21,35 @@
     <!-- 本文ブロック -->
     <div class="container" style="background:#;height:auto;">
         <h1 class="f-title">メンバー紹介</h1>
+        <h2>准教授</h2>
+        <div class="row">
+            <div class="container">
+                <div class="row">
+                    <div class="col-xs-1">
+                        Boss
+                    </div>
+                    <div class="col-xs-7 col-md-5">
+                        <div class="col-md-4 col-sm-5 col-xs-12">
+                            片山 徹郎
+                        </div>
+                        <div class="col-md-8 col-sm-7 col-xs-12">
+                            Tetsuro KATAYAMA
+                        </div>
+                    </div>
+                    <div class="col-xs-4 col-md-2">
+                        <a href="http://earth.cs.miyazaki-u.ac.jp/~kat/">Home page</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <h2>修士</h2>
+        <?php include (dirname(__FILE__).'/parts/member_list/master.html'); ?>
+
+        <!-- ヘッダー -->
+        <h2>学士</h2>
+        <?php include (dirname(__FILE__).'/parts/member_list/bachelor.html'); ?>
     </div>
-    <p></p>
 
     <!-- フッター -->
     <?php include (dirname(__FILE__).'/parts/footer.html'); ?>
