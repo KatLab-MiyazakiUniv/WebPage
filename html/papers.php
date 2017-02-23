0a83236bc286945cb97b7aa630e0b073312f9b83
Futa Hirakoba <korosuke613613@gmail.com>
2017-02-23 12:38:30 +0900

    論文リスト、イベントのページを作成（作成だけ）

---
+++ html/papers.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<!-- 共通<head>タグ -->
+<?php include (dirname(__FILE__).'/parts/head_tag.html'); ?>
+
+
+<head>
+    <title>メンバー紹介 - 片山(徹)研究室 : 宮崎大学</title>
+</head>
+
+
+<body>
+
+    <!-- ヘッダー -->
+    <?php include (dirname(__FILE__).'/parts/header.html'); ?>
+
+    <!-- メニュー -->
+    <?php include (dirname(__FILE__).'/parts/menu.html'); ?>
+
+    <!-- 本文ブロック -->
+    <div class="container f-rounded" style="background:#;height:auto;">
+        <h1 class="f-title">論文リスト</h1>
+        工事中
+    </div>
+
+    <!-- フッター -->
+    <?php include (dirname(__FILE__).'/parts/footer.html'); ?>
+
+    <!-- Javascriptの読み込み -->
+    <?php include (dirname(__FILE__).'/parts/js_load.html'); ?>
+
+</body>
+
+</html>
