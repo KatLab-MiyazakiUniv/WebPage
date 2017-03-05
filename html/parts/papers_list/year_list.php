8ea87a628f95068995ace263a039d891fbfaa79c
Futa Hirakoba <korosuke613613@gmail.com>
2017-03-06 02:20:24 +0900

    現在の年リンクを押せないようにした

--- html/parts/papers_list/year_list.php
+++ html/parts/papers_list/year_list.php
@@ -14,7 +14,11 @@
 
     echo '<ul class="list-inline">';
     for($i = $last_year; $i>=$first_year; $i--){
-        echo '<li><a href="?year=', $i, '">', $i, '年', '</a></li>';
+        if($year == $i){
+            echo '<li><a style="color:#000000;pointer-events:none;cursor:default;text-decoration:none;">', $i, '年</a></li>';
+        }else{
+            echo '<li><a href="?year=', $i, '">', $i, '年</a></li>';    
+        }
     }
     echo '</ul>';
 
