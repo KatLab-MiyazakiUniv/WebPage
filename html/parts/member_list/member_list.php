e91880d3748cee8c986eafea567b6a263289a4d3
Futa Hirakoba <korosuke613613@gmail.com>
2017-04-08 02:51:06 +0900

    人物の情報を追加

--- html/parts/member_list/member_list.php
+++ html/parts/member_list/member_list.php
@@ -2,13 +2,22 @@
 
 $masters = array(
     //ここにメンバーを追加
+    '黄 一諾'=>array('M2', '', ''),
+    '佐藤 拓弥'=>array('M2', 'Takuya SATOU', ''),
+    '立山 博基'=>array('M2', 'Hiroki TATIYAMA', ''),
     '田上 諭'=>array('M2', 'Satoshi TANOUE', 'http://earth.cs.miyazaki-u.ac.jp/~tanoue/')
+    '村上 雅人'=>array('M1', 'Masato MURAKAMI', ''),
+    '森 敬介'=>array('M1', 'Keisuke MORI', '')
 );
 
 $bachelors = array(
     //ここにメンバーを追加
     '市山 力也'=>array('B4', 'Rikiya ICHIYAMA', ''),
-    '平木場 風太'=>array('B4', 'Futa HIRAKOBA', 'https://korosuke613.github.io/')
+    '大久保 圭得'=>array('B4', '', ''),
+    '佐竹 侑斗'=>array('B4', 'Yuuto SATAKE', ''),
+    '平木場 風太'=>array('B4', 'Futa HIRAKOBA', 'https://korosuke613.github.io/'),
+    '宮地 俊宏'=>array('B4', 'Toshihiro MIYAJI', ''),
+    '山脇 幸士'=>array('B4', 'Takashi YAMAWAKI', '')
 );
 
 ?>
