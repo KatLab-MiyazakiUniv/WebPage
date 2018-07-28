cc6a727fb4787b8aaf9aab1062eb22fe164b8a06
Futa HIRAKOBA <korosuke613@users.noreply.github.com>
2018-07-29 00:47:31 +0900

    Update hook.php

--- hook.php
+++ hook.php
@@ -3,12 +3,14 @@
 <?php
 
 $LOG_FILE = dirname(__FILE__).'/hook.log';
+
+// $WEBPAGE_SECRET_KEYはapache環境変数に記述
 $SECRET_KEY = getenv('WEBPAGE_SECRET_KEY');
 
 if ( isset($_GET['key']) && $_GET['key'] === $SECRET_KEY && isset($_POST['payload']) ) {
     $payload = json_decode($_POST['payload'], true);
     if ($payload['ref'] === 'refs/heads/master') {
-        `git pull origin master`;
+        echo shell_exec("git pull");
         file_put_contents($LOG_FILE, date("[Y-m-d H:i:s]")." ".$_SERVER['REMOTE_ADDR']." git pulled: ".$payload['head_commit']['message']."\n", FILE_APPEND|LOCK_EX);
     }
 } else {
