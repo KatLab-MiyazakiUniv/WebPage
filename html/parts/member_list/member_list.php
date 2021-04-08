02ac6caf2e565423ba3a7767a65325d58bddfa3e
Hisataka Hagiyama <hisataka7@icloud.com>
2021-04-08 11:13:02 +0900

    Update: メンバーリストを更新

--- html/parts/member_list/member_list.php
+++ html/parts/member_list/member_list.php
@@ -2,22 +2,27 @@
 
 $doctors = array(
     //ここにメンバーを追加
-    'チャンドラ'=>array('D2', 'MOHAMAD CHANDRA SAPTRA', ''),
-    'バユ'=>array('D1', 'BAYU PRIYAMBADHA', '')
+    'チャンドラ'=>array('D3', 'MOHAMAD CHANDRA SAPTRA', ''),
+    'バユ'=>array('D2', 'BAYU PRIYAMBADHA', '')
 );
 
 $masters = array(
     //ここにメンバーを追加
-    '執行 泰弘'=>array('M2', 'Yasuhiro SHIGYO', ''),
-    '有馬 薫'=>array('M1', 'Kaoru ARIMA', '')
+    '有馬 薫'=>array('M2', 'Kaoru ARIMA', ''),
+    '上田 高寛'=>array('M1', 'Takahiro UEDA', 'https://takahiro55555.github.io/'),
+    '菅 健将'=>array('M1', 'Kensuke SUGA', ''),
+    '前畑 有希'=>array('M1', 'Yuki MAEHATA', ''),
+    '武藤 崇史'=>array('M1', 'Takafumi MUTO', ''),
 );
 
 $bachelors = array(
     //ここにメンバーを追加
-    '上田 高寛'=>array('B4', 'Takahiro UEDA', 'https://takahiro55555.github.io/'),
-    '菅 健将'=>array('B4', 'Kensuke SUGA', ''),
-    '前畑 有希'=>array('B4', 'Yuki MAEHATA', ''),
-    '武藤 崇史'=>array('B4', 'Takafumi MUTO', ''),
+    '内山 大和'=>array('B4', 'Yamato UCHIYAMA', ''),
+    '児玉 純輝'=>array('B4', 'Junki KODAMA', ''),
+    '田中 佑和'=>array('B4', 'Yuto TANAKA', ''),
+    '萩山 恒威'=>array('B4', 'Hisataka HAIGYAMA', ''),
+    '樋口 裕翔'=>array('B4', 'Hiroto HIGUCHI', ''),
+    '宮下 丈明'=>array('B4', 'Takeaki MIYASHITA', ''),
 );
 
 ?>
