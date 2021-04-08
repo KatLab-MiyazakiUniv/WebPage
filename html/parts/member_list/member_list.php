39ff196bbffae35c88f42eb150a4760cec396c25
Hisataka Hagiyama <hisataka7@icloud.com>
2021-04-08 12:18:55 +0900

    Update: メンバーリストのミスを修正

--- html/parts/member_list/member_list.php
+++ html/parts/member_list/member_list.php
@@ -11,7 +11,6 @@ $masters = array(
     '有馬 薫'=>array('M2', 'Kaoru ARIMA', ''),
     '上田 高寛'=>array('M1', 'Takahiro UEDA', 'https://takahiro55555.github.io/'),
     '菅 健将'=>array('M1', 'Kensuke SUGA', ''),
-    '前畑 有希'=>array('M1', 'Yuki MAEHATA', ''),
     '武藤 崇史'=>array('M1', 'Takafumi MUTO', ''),
 );
 
@@ -20,7 +19,7 @@ $bachelors = array(
     '内山 大和'=>array('B4', 'Yamato UCHIYAMA', ''),
     '児玉 純輝'=>array('B4', 'Junki KODAMA', ''),
     '田中 佑和'=>array('B4', 'Yuto TANAKA', ''),
-    '萩山 恒威'=>array('B4', 'Hisataka HAIGYAMA', ''),
+    '萩山 恒威'=>array('B4', 'Hisataka HAGIYAMA', ''),
     '樋口 裕翔'=>array('B4', 'Hiroto HIGUCHI', ''),
     '宮下 丈明'=>array('B4', 'Takeaki MIYASHITA', ''),
 );
