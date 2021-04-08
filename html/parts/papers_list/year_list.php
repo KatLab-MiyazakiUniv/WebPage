384d4b42916068c68c775836ed12318d4ac29ca3
Hisataka Hagiyama <hisataka7@icloud.com>
2021-04-08 11:28:43 +0900

    Update: 2021年の論文リストまで表示するように変更

--- html/parts/papers_list/year_list.php
+++ html/parts/papers_list/year_list.php
@@ -1,14 +1,14 @@
 <?php
 
-    $first_year = 2013; 
-    $last_year = 2020;
+    $first_year = 2013;
+    $last_year = 2021;
 
     if (isset($_GET["year"]) && (int)$_GET["year"] >= $first_year && (int)$_GET["year"] <= $last_year) {
         $year = (int)$_GET["year"];
     } else {
         $year = $last_year;
     }
-  
+
 
 
 
@@ -17,7 +17,7 @@
         if($year == $i){
             echo '<li><a style="color:#000000;pointer-events:none;cursor:default;text-decoration:none;">', $i, '年</a></li>';
         }else{
-            echo '<li><a href="?year=', $i, '">', $i, '年</a></li>';    
+            echo '<li><a href="?year=', $i, '">', $i, '年</a></li>';
         }
     }
     echo '</ul>';
