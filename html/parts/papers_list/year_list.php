db5022a4658743c5dbd4244bf9b6ad142ac750d9
YuyaKimura <molpui0726@gmail.com>
2025-03-28 12:16:36 +0900

    add: 2025年版の論文リストを追加

--- html/parts/papers_list/year_list.php
+++ html/parts/papers_list/year_list.php
@@ -1,25 +1,26 @@
 <?php
 
-    $first_year = 2013;
-    $last_year = 2024;
+$first_year = 2013;
+$last_year = 2025;
 
-    if (isset($_GET["year"]) && (int)$_GET["year"] >= $first_year && (int)$_GET["year"] <= $last_year) {
-        $year = (int)$_GET["year"];
+if (isset($_GET["year"]) && (int) $_GET["year"] >= $first_year && (int) $_GET["year"] <= $last_year) {
+    $year = (int) $_GET["year"];
+} else {
+    $year = $last_year;
+}
+
+
+
+
+echo '<ul class="list-inline">';
+for ($i = $last_year; $i >= $first_year; $i--) {
+    if ($year == $i) {
+        echo '<li><a style="color:#000000;pointer-events:none;cursor:default;text-decoration:none;">', $i, '年</a></li>';
     } else {
-        $year = $last_year;
+        echo '<li><a href="?year=', $i, '">', $i, '年</a></li>';
     }
-
-
-
-
-    echo '<ul class="list-inline">';
-    for($i = $last_year; $i>=$first_year; $i--){
-        if($year == $i){
-            echo '<li><a style="color:#000000;pointer-events:none;cursor:default;text-decoration:none;">', $i, '年</a></li>';
-        }else{
-            echo '<li><a href="?year=', $i, '">', $i, '年</a></li>';
-        }
-    }
-    echo '</ul>';
+}
+echo '</ul>';
 
 ?>
+
